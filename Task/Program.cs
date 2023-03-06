6208e990c1cbd0832a2a914e927eadd74799b0af
Kirill <munnin95@gmail.com>
2023-03-06 16:30:55 +0300

    Complete task

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array1 = new string[9] {"Hello", "GB", "that","is", "my", "work", "ty","gl","=)"};
+string[] array2 = new string[array1.Length];
+
+
+void FillSecondArray(string[] array1, string[] array2)
+{
+    int indexarray2 = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[indexarray2] = array1[i];
+        indexarray2++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillSecondArray(array1, array2);
+PrintArray(array2);
